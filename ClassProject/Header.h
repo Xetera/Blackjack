25b756236b9c285d4ffe28527e82839d12dded3e
Ali <mythologyali@gmail.com>
2018-01-17 20:52:02 -0800

    initial commit

---
+++ ClassProject/Header.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include<string>
+
+std::string Suit[4] { "Diamonds", "Spades" , "Hearts" , "Clubs"};
+
+/*
+
+class Card {
+	suit generateRandomSuit();
+	int generateRandomNumber();
+};
+*/
+
